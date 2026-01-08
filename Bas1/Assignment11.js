eda41d1e31a12a9ddcdfde6c4b299cd32438bdaf
jitesh kumar <jitesh.kumar05official@gmail.com>
2026-01-08 16:46:58 +0530

    assignment completed

--- Bas1/Assignment11.js
+++ Bas1/Assignment11.js
@@ -33,10 +33,22 @@ console.log(message);
 
 // Tasks:
 //    1. If price < 500 → "Budget Course"
+message = "";
+if(price < 500){
+    message = "Budget Course";
+}
 //    2. If price between 500–1000 → "Standard Course"
+else if(price < 1000){
+    message = "Standard Course";
+}
 //    3. If price > 1000 → "Premium Course"
+else{
+    message = "Premium Course";
+}
 //    4. Store label in courseTag
+let courseTag = message;
 //    5. Print the label
+console.log(courseTag);
 
 
 
