84343e466b41dd8ffd3111a15e00108b7187cbaf
jitesh kumar <jitesh.kumar05official@gmail.com>
2026-01-08 16:52:48 +0530

    assignment completed

--- Bas1/Assignment11.js
+++ Bas1/Assignment11.js
@@ -55,12 +55,22 @@ console.log(courseTag);
 // HANDS-ON 3: Enrollment Eligibility Checker
 // ------------------------------------------
 // Initial data:
-//     let hasPaid = true;
-//     let hasCompletedBasics = false;
+    let hasPaid = true;
+    let hasCompletedBasics = false;
 
 // Tasks:
 //    1. If both conditions are true → "Enroll Now"
+let enrollMessage = "";
+if(hasPaid && hasCompletedBasics){
+    enrollMessage = "Enroll Now";
+}
 //    2. Otherwise → "Complete Requirements"
+else{
+    enrollMessage = "Complete Requirements";
+}
 //    3. Use ternary operator
+let res = ((hasPaid) && (enrollMessage)) ? "Enroll Now" : "Complete Requirements";
 //    4. Store result in enrollMessage
 //    5. Print message
+console.log(enrollMessage);
+console.log(res);
