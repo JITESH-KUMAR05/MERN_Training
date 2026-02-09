cf527ac06bbb8c780c1bc1b96e0e423e3f3e803f
jitesh <jitesh.kumar05official@gmail.com>
2026-02-09 13:12:14 +0530

    designed middlewares

--- Week5/1_blog_app_backend/middlewares/validAuthor.js
+++ Week5/1_blog_app_backend/middlewares/validAuthor.js
@@ -6,8 +6,17 @@ export const validAuthor = async(req,res,next) => {
     let authorId = req.body?.author || req.params?.authorid;
     // verify the author
     let isMatchUser = await UserTypeModel.findById(authorId);
-    if(!isMatchUser || isMatchUser.role != "AUTHOR"){
+    // if author not found
+    if(!isMatchUser){
         return res.status(404).json({message:"author is not valid"})
+    }
+    // if author founcd but role is differnt 
+    if(isMatchUser.role != "AUTHOR"){
+        return res.status(403).json({message:"User is not Author"})
+    }
+    // if author is there but blocked
+    if(!isMatchUser.isActive){
+        return res.status(403).json({message:"Author account is not active contact Admin!"})
     }
     // give to the next middleware
     next();
