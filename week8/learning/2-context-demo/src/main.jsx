37bda069932ff26592e88f3ec4f0a8c25ac57672
jitesh <jitesh.kumar05official@gmail.com>
2026-03-03 19:42:05 +0530

    week 8 done

--- week8/learning/2-context-demo/src/main.jsx
+++ week8/learning/2-context-demo/src/main.jsx
@@ -3,13 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import CounterContextProvider from './contexts/CounterContextProvider.jsx'
+import UserContextProvider from './contexts/UserContextProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   
-    
-    <CounterContextProvider>
-      {/* wrap root component with provider */}
-        <App />
-    </CounterContextProvider>
+    <UserContextProvider>
+      <CounterContextProvider>
+        {/* wrap root component with provider */}
+          <App />
+      </CounterContextProvider>
+    </UserContextProvider>
   
 )
