085fd8a42415d9020dd1fb069c5056e64235571b
jitesh <jitesh.kumar05official@gmail.com>
2026-03-15 19:01:10 +0530

    article

---
+++ Week10/BlogApp/frontend/src/App.jsx
@@ -0,0 +1,70 @@
+import { useState } from 'react'
+import {createBrowserRouter, RouterProvider} from "react-router"
+import toast, { Toaster } from 'react-hot-toast';
+import './App.css'
+import RootLayout from './components/RootLayout'
+import Home from './components/Home'
+import Register from './components/Register'
+import Login from './components/Login'
+import AddArticle from './components/AddArticle'
+import UserProfile from './components/UserProfile'
+import AuthorProfile from './components/AuthorProfile';
+import Article from './components/Article';
+
+function App() {
+  const [count, setCount] = useState(0)
+  const routerObj = createBrowserRouter([
+    {
+      path:"/",
+      element:<RootLayout />,
+      children:[
+        {
+          path:"/",
+          element: <Home />
+        },
+        {
+          path:"register",
+          element: <Register />
+        },
+        {
+          path:"login",
+          element: <Login />
+        },
+        {
+          path:"add-article",
+          element:<AddArticle />
+        },
+        {
+          path:"user-profile",
+          element: <UserProfile />,
+          children:[
+            
+          ]
+        },
+        {
+          path:"author-profile",
+          element:<AuthorProfile />,
+          children:[
+            
+          ]
+        },
+        {
+          path:"article/:id",
+          element: <Article />
+        },
+        {
+          path:"dashboard",
+          element:<AuthorProfile />
+        }
+      ]
+    }
+  ]);
+  return (
+    <>
+      <Toaster position='top-center' />
+    <RouterProvider router={routerObj}></RouterProvider>
+    </>
+  )
+}
+
+export default App
---
+++ Week10/BlogApp/frontend/src/components/Article.jsx
@@ -0,0 +1,48 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { useLocation, useParams } from 'react-router'
+import { errorClass, loadingClass } from '../styles/common';
+
+const Article = () => {
+  const {id} = useParams();
+  const locationObj = useLocation();
+  // console.log(locationObj)
+  const [article,setArticle] = useState(locationObj?.state?.articleObj || null);
+  const [loading, setLoading] = useState(!article);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    // if article is not present then we have to make an api call
+    if(!article){
+      const fetchArticle = async() => {
+        try {
+          const resObj = await axios.get(`http://localhost:5000/user-api/article/${id}`);
+          const res = resObj.data.payload;
+          setArticle(res);
+
+        } catch (err) {
+          setError(err);
+        }finally{
+          setLoading(false);
+        }
+      }
+      fetchArticle();
+    }
+  },[id,article])
+
+  if(loading){
+    return <p className={loadingClass}></p>
+  }
+  if(error){
+    return <p className={errorClass}>{error.message}</p>
+  }
+  
+  return (
+    <div>
+      <h1>{article.title}</h1>
+      <p>{article.content}</p>
+    </div>
+  )
+}
+
+export default Article
